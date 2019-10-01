3c05ede8a46313f13ba450eb10be23d76eef57e6
Andre Lee <andreleecoder@gmail.com>
2019-10-01 10:54:25 -0300

    trypescript lesson easy peasy

---
+++ helloTypescript/main.js
@@ -0,0 +1,24 @@
+//variavei
+var minhavar = "minha primeira variavel";
+function minhafunc(x, y) {
+    return x + y;
+}
+var num = 2;
+var PI = 3.14;
+//funçoes
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 3; });
+//classes
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+//tipagem de variaveis
+var n1 = "sdfesfddf";
---
+++ helloTypescript/main.ts
@@ -0,0 +1,26 @@
+//variavei
+var minhavar="minha primeira variavel"
+
+function minhafunc(x,y) {
+return x+y
+    
+}
+
+let num=2
+const PI=3.14
+//funçoes
+var numeros=[1,2,3]
+numeros.map(function(valor){
+
+    return valor*2
+})
+numeros.map(valor =>  valor*3)
+//classes
+class Matematica{
+    soma(x,y){
+        return x+y
+    }
+}
+//tipagem de variaveis
+var n1="sdfesfddf"
+
