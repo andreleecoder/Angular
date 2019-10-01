996fdc5928ee6e0687d34a415f1d6e1848011d0d
Andre Lee <andreleecoder@gmail.com>
2019-10-01 09:59:09 -0300

    second component created

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,13 @@
+import{Component}from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+@Component({
+    selector:'meu-primeiro-component',
+    template:`
+    <p>Meu primeiro component com Angular !</p>
+    `
+
+})
+
+export class MeuPrimeiroComponent {
+    
+}
