f104e65f8b8718517dac415ba12fb13b6924e579
Andre Lee <andreleecoder@gmail.com>
2019-10-04 22:03:05 -0300

    templates and services

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
-
-  constructor() { }
+  nomePortal: string=''
+  cursos: string[] 
+  constructor(private CursosService:CursosService) { 
+    this.nomePortal='http://loiane.training'
+    this.cursos= this.CursosService.getCursos()
+  }
 
   ngOnInit() {
   }
