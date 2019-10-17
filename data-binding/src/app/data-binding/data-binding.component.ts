8077569abd83424ed9b18a0bfa806a006a3a5141
Andre Lee <andreleecoder@gmail.com>
2019-10-17 04:40:32 -0300

    event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -9,12 +9,31 @@ export class DataBindingComponent implements OnInit {
   url ='http://loiane.com'
   cursoAngular = true
   urlImagem='http://lorempixel.com/400/200/nature/'
+  
+  valorAtual:string='';
+  valorSalvo
+  isMouseOver:boolean=false
 
   getValor(){
     return 1
   }
   getCurtirCurso(){
     return true
+  }
+  botaoClicado(){
+    alert('Botão clicado')
+  }
+  onKeyUp(evento:KeyboardEvent){
+    this.valorAtual=(<HTMLInputElement>evento.target).value
+    
+
+  }
+  salvarValor(valor){
+    this.valorSalvo=valor;
+
+  }
+  onMouseOverNOut(){
+    this.isMouseOver = !this.isMouseOver
   }
   
   constructor() { }
